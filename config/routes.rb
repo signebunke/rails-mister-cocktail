b58cfaa33a21c6a24495f92c3ea97b4f0c674fa8
Signe Bunke <signebunke@gmail.com>
2021-02-02 16:47:45 +0100

    models created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
   resources :cocktails, only: [:index, :create, :new, :show] do
     resources :doses, only: [:create, :new, :destroy]
   end
+  resources :doses, only: [:destroy]
 end
